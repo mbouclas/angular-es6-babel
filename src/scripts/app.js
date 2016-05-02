2d8128ae14a09e1a3423d1b2eb3c491feaa0636b
Michael Bouclas <mbouclas@gmail.com>
2016-05-02 21:47:49 +0300

    Initial commit

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,7 +1,7 @@
-var angular = require('angular');
-
+import 'angular';
 import UserController from './User/UserController';
 
 angular.module('myApp', [])
     .controller('UserController', UserController);
+
 
